3d80ea0ece2e9556ce8046808f4ee10a4ebc8b29
glen03xs <glen03xs@gmail.com>
2018-03-20 14:49:55 +0800

    adjust the active / focus label underline color

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@ const todoInput = document.getElementById('todo');
 
 // Load Event Listener
 loadEventListeners();
-
+// Load All Event Listener
 function loadEventListeners() {
+	// DOM  Load Event
+	document.addEventListener('DOMContentLoaded', getTodos);
 	// Add Todo Event
 	form.addEventListener('submit', addTodo);
 
@@ -20,6 +22,37 @@ function loadEventListeners() {
 
 	// Filter Todo's Event
 	filter.addEventListener('keyup', filterTodo);
+}
+
+//Get Todos from Local Storage
+function getTodos() {
+	let todos;
+	if (localStorage.getItem('todos') === null) {
+		todos = [];
+	} else {
+		todos = JSON.parse(localStorage.getItem('todos'));
+	}
+
+	todos.forEach(function(todo) {
+	// Create li Element
+	const li = document.createElement('li');
+	li.className = 'collection-item';
+	
+	// Create text node and append to li
+	li.appendChild(document.createTextNode(todo));
+	
+	// Create New Link Element
+	const link = document.createElement('a');
+	link.className = 'delete-item secondary-content';
+	link.innerHTML = '<i class="fa fa-remove"></li>';
+	
+	// Append the Link to the LI
+	li.appendChild(link);
+	
+	// Append li to ul
+	todoList.appendChild(li);		
+	});
+
 }
 
 // Add Todo 
@@ -46,27 +79,71 @@ function addTodo(e) {
 	// Append li to ul
 	todoList.appendChild(li);
 
+	// Store to Local Storage
+	storeTaskInLocalStorage(todoInput.value);
+
 	// Clear Input
 	todoInput.value = '';
 
 	e.preventDefault();
 }
 
+// Store Todo
+	function storeTaskInLocalStorage(todo) {
+		let todos;
+		if (localStorage.getItem('todos') === null) {
+			todos = [];
+		} else {
+			todos = JSON.parse(localStorage.getItem('todos'));
+		}
+
+		todos.push(todo);
+
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}
+
 // Delete Todo
 function deleteTodo(e) {
 	if (e.target.parentElement.classList.contains('delete-item')) {
 		if(confirm('Do You Want to Delete Item?')) {
 			e.target.parentElement.parentElement.remove();
+
+			// Remove from Local Storage
+			removeTaskFromLocalStorage(e.target.parentElement.parentElement);
 		}
 	}
+}
+
+// Delete from Local Storage
+function removeTaskFromLocalStorage(todoItem) {
+	let todos;
+	if (localStorage.getItem('todos') === null) {
+		todos = [];
+	} else {
+		todos = JSON.parse(localStorage.getItem('todos'));
+	}
+
+	todos.forEach(function(todo, index) {
+		if(todoItem.textContent === todo)
+			todos.splice(index, 1);
+	});
+
+	localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 // Delete All Todo
 function deleteAllTodos() {
 	while(todoList.firstChild) {
-		todoList.removeChild(taskList.firstChild);
+		todoList.removeChild(todoList.firstChild);
 	}
+
+	// Delete All from Local Storage
+	clearTaskFromLocalStorage();
 }
+
+	function clearTaskFromLocalStorage() {
+		localStorage.clear();
+	 }
 
 // Filter Todo's
 function filterTodo(e) {
